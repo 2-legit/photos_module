1caa9f8c1a0089833f53fe6447bb7d0cb825cd30
Kristoffer U Enriquez <enriquezkristoffer@yahoo.com>
2019-03-28 17:18:22 -0700

    Adds connection.js and pass_example.js in database folder, creates connection to MySQL service

---
+++ server/database/pass_example.js
@@ -0,0 +1,5 @@
+const DB_PASS = 'change this password';
+// Change this password to the correct password and save a copy with the name `pass.js`
+// Note that the password may be different across hardware configurations
+
+module.exports = DB_PASS;
