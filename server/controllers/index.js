3df2c20ed53c30fe73c7ef5b2fb384c8146b8f9c
Kristoffer U Enriquez <enriquezkristoffer@yahoo.com>
2019-03-30 14:25:35 -0700

    Removes empty line breaks from code, adds some additional comments

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,18 +1,12 @@
 const Photo = require('../model/photos.js');
 
 module.exports = {
-
   photos: {
-
     get: (req, res) => {
-
       Photo.findAll({ where: { roomid: req.params.roomid } })
       .then((result) => {
         res.send({ photos: result });
       });
-
     }
-
   }
-
 }
