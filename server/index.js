2387d0f0ee2dd9db7a142143c41822ed67cc040c
Kristoffer U Enriquez <enriquezkristoffer@yahoo.com>
2019-03-29 18:25:03 -0700

    Adds controllers/index.js, router/index.js to server to hold routing logic and route handlers, creates model in photos.js

--- server/index.js
+++ server/index.js
@@ -1,13 +1,18 @@
 const express = require('express');
+const initializeDatabase = require('./database/init.js');
 
 const app = express();
+const router = require('./router');
 
 app.use(express.static('public'));
+
+app.use('/photos', router);
 
 const port = 3000;
 
 app.listen(port, () => {
   console.log('[Server] Now listening on port', port);
-
-  require('./database/init.js');
+  initializeDatabase();
 });
+
+module.exports = express.Router();
--- server/model/photos.js
+++ server/model/photos.js
@@ -1,1 +1,20 @@
-const sequelize = require('./init.js');
+const Sequelize = require('sequelize');
+const sequelize = require('../database/connection.js');
+
+const Model = sequelize.define('Model', {
+  id: { type: Sequelize.INTEGER, primaryKey: true },
+  list: Sequelize.INTEGER,
+  roomid: Sequelize.INTEGER,
+  imageurl: Sequelize.STRING(150),
+  imagedesc: Sequelize.STRING(150)
+});
+
+class Photo extends Model {}
+Photo.init({
+  list: Sequelize.INTEGER,
+  roomid: Sequelize.INTEGER,
+  imageurl: Sequelize.STRING(150),
+  imagedesc: Sequelize.STRING(150)
+}, { sequelize });
+
+module.exports = Photo;
