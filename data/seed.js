1ae31090a4648df564620b8b30c81a8efb3c2622
Kristoffer U Enriquez <enriquezkristoffer@yahoo.com>
2019-04-01 12:26:10 -0700

    Remove require call from promise chain, remove single .then statement from promise chain

--- data/seed.js
+++ data/seed.js
@@ -1,21 +1,19 @@
 const faker = require('faker');
 const database = require('../server/database/connection.js');
 const initialize = require('../server/database/init.js');
+const Photo = require('../server/model/photos.js');
 
 const randomNumber = max => Math.ceil(Math.random() * max);
 
 const seedDatabase = (rooms) => {
   // Drop the database to clear all prior information
   database.query('DROP DATABASE IF EXISTS photo;')
-    .then(() => // Create the database again and create the table
-      initialize())
-    .then(() => // Create the database model
-      require('../server/model/photos.js'))
-    .then((Photo) => { // Generate test instances in an array
+    .then(() => initialize()) // Create the database again and create the table
+    .then(() => { // Generate test instances in an array
       const array = [];
-      for (let i = 0; i < rooms; i++) {
+      for (let i = 0; i < rooms; i += 1) {
         const numberOfPictures = randomNumber(10);
-        for (let p = 0; p < numberOfPictures; p++) {
+        for (let p = 0; p < numberOfPictures; p += 1) {
           array.push({
             list: p,
             roomid: i,
