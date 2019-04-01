d15369bf1551ac1d940a2dad2198fa17d43372c5
Kristoffer U Enriquez <enriquezkristoffer@yahoo.com>
2019-04-01 14:02:54 -0700

    Add test folder with client.test.js and server.test.js, add test:all script to package.json

---
+++ test/server.test.js
@@ -0,0 +1,12 @@
+/**
+ * @jest-environment node
+ */
+
+ /**
+  * Server test script
+  * Tests server functionality in node-based environment
+  */
+
+  test('should not have access to document object', () => {
+    expect(typeof document).toBe('undefined');
+  })
