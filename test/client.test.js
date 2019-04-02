5cf426c7c5f6f98427ea8c2c78d9b48a375e04a0
Kristoffer U Enriquez <enriquezkristoffer@yahoo.com>
2019-04-01 17:31:28 -0700

    Configure webpack to compile client.test.js to allow enzyme to run in conjunction with jest
    - Remove `globals` property from .eslintrc.js
    - Add jest-environment-enzyme to developer dependencies
    - Add individual test scripts to package.json for both server and client
    - Add webpack.test.config.js to configure webpack specifically to compile test file

--- test/client.test.js
+++ test/client.test.js
@@ -1,12 +1,24 @@
 /**
- * @jest-environment enzyme
- */
-
-/**
  * Client test script
  * Tests client functionality in a window-like environment with Enzyme testing suite
  */
 
+import React from 'react';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
+
 test('should have access to document object', () => {
   expect(typeof document).toBe('object');
+});
+
+test('should have access to enzyme', () => {
+  expect(typeof Enzyme.shallow).toBe('function');
+});
+
+test('should be able to mock-render jsx expressions', () => {
+  const element = Enzyme.shallow(<h1>Hello world!</h1>);
+
+  expect(element).not.toBe(undefined);
 });
