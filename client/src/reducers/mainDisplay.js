6762abaef601e629944cc89e2fb8989f4b238c4d
Kristoffer U Enriquez <enriquezkristoffer@yahoo.com>
2019-04-03 21:46:20 -0700

    Write comments outlining reducer functionality

---
+++ client/src/reducers/mainDisplay.js
@@ -0,0 +1,19 @@
+/**
+ * mainDisplay.js
+ * Reducer for actions concerned with affecting change to the main display
+ */
+
+// TODO:
+// Write a reducer that accepts the following actions and handles them accordingly:
+
+// CHECK_WIDTH
+// payload: [window.innerWidth]
+// affects: mainDisplayMode: {'FULLSIZE'|'MIDSIZE'|'COMPACT'}
+
+// FOCUS_IMAGE
+// payload: [index]
+// affects: inFocus: {0|1|2|3|4|5}
+
+// DEFOCUS_ALL
+// no payload
+// affects: inFocus: {'NONE'}
