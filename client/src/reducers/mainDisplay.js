d20289f398dbfcf7602de508ee887bb2eb7d93df
Kristoffer U Enriquez <enriquezkristoffer@yahoo.com>
2019-04-04 16:54:16 -0700

    Remove comments from mainDisplay and reducer test

--- client/src/reducers/mainDisplay.js
+++ client/src/reducers/mainDisplay.js
@@ -3,9 +3,6 @@
  * Reducer for actions concerned with affecting change to the main display
  */
 
-// TODO:
-// Write a reducer that accepts the following actions and handles them accordingly:
-
 const initialState = {
   photos: [],
   displayMode: 'FULLSIZE',
@@ -16,15 +13,11 @@ function mainDisplay(state = initialState, action) {
   const TYPE = action !== undefined && action !== null ? action.type : 'INVALID';
 
   let newState;
-
   let displayMode;
   let inFocus;
 
   switch (TYPE) {
     case 'CHECK_WIDTH':
-      // payload: [window.innerWidth]
-      // affects: mainDisplayMode: {'FULLSIZE'|'MIDSIZE'|'COMPACT'}
-
       if (action.width >= 1200) {
         displayMode = 'FULLSIZE';
       } else if (action.width < 1200 && action.width >= 750) {
@@ -32,16 +25,11 @@ function mainDisplay(state = initialState, action) {
       } else {
         displayMode = 'COMPACT';
       }
-
       newState = Object.assign({}, state, {
         displayMode,
       });
       return newState;
-
     case 'FOCUS_IMAGE':
-      // payload: [index]
-      // affects: inFocus: {0|1|2|3|4|5}
-
       if (state.displayMode === 'FULLSIZE' && action.viewpane <= 4) {
         inFocus = action.viewpane;
       } else if (state.displayMode === 'MIDSIZE' && action.viewpane <= 2) {
@@ -53,16 +41,12 @@ function mainDisplay(state = initialState, action) {
         inFocus,
       });
       return newState;
-
     case 'DEFOCUS_ALL':
-      // no payload
-      // affects: inFocus: {'NONE'}
       inFocus = 'NONE';
       newState = Object.assign({}, state, {
         inFocus,
       });
       return newState;
-
     default:
       return state;
   }
--- test/redux-store/Reducers.test.js
+++ test/redux-store/Reducers.test.js
@@ -7,10 +7,8 @@ import photoApp from '../../client/src/reducers/index';
 import actionCreators from '../../client/src/actionCreators/index';
 import initialState from '../../client/src/reducers/initialState';
 
-// Destructure action creator functions from actionCreators
 const { checkWidth, focusImage, defocusAll, toggleModal, modalSelect } = actionCreators;
 
-// Create a function to produce a state object with properties mapped to arguments
 function mockState(photos, displayMode, inFocus, modal, modalDisplay) {
   const state = {
     mainDisplay: {
@@ -28,177 +26,120 @@ function mockState(photos, displayMode, inFocus, modal, modalDisplay) {
 
 // Describe: default reducer behaviors
 describe('default reducer behaviors', () => {
-  // test: should set state to initial state if state is undefined
   test('should set state to initial state if state is undefined', () => {
-    // pass into photoApp null
     const newState = photoApp();
-    // assert that the return value and the initialState are deeply equal
     expect(newState).toEqual(initialState);
   });
 
-  // test: should return the input state if no actions were taken
   test('should return the input state if no actions were taken', () => {
-    // create a working state object
     const workingState = mockState([{}], 'FULLSIZE', 0, false, 'NONE');
-    // pass into photoApp a working state and null
     const newState = photoApp(workingState, null);
-    // assert that the return value and declared state object are the same object
     expect(newState).toBe(workingState);
   });
 
-  // test: should return previous state if reducer was passed an invalid action
   test('should return previous state if reducer was passed an invalid action', () => {
-    // create a working state object
     const workingState = mockState([{}], 'FULLSIZE', 0, false, 'NONE');
-    // pass into photoApp a working state and an invalid action object
     const newState = photoApp(workingState, { type: 'INVALID', payload: 'oops' });
-    // assert that the return value and declared state object are the same object
     expect(newState).toBe(workingState);
   });
 
-  // test: should not mutate the input state
   test('should not mutate the input state', () => {
-    // create a working state object
     const workingState = mockState([{}], 'FULLSIZE', 0, false, 'NONE');
-    // pass into photoApp a working state and a valid action object
     const newState = photoApp(workingState, { type: 'DEFOCUS_ALL' });
-    // assert that the return value and the declared state object are not the same object
     expect(newState).not.toBe(workingState);
   });
 
 });
 
-// Describe: mainDisplay
 describe('mainDisplay', () => {
 
-  // Describe: CHECK_WIDTH
   describe('CHECK_WIDTH', () => {
-    // test: should save to state 'FULLSIZE' for widths greater than 1200px
     test('should save to state \'FULLSIZE\' for widths greater than 1200px', () => {
-      // change window.innerWidth to a resolution greater than 1200px
       window.innerWidth = 1300;
-      // invoke action creator for CHECK_WIDTH
       const CHECK_WIDTH = checkWidth();
-      // pass into photoApp the initial state and action
       const newState = photoApp(undefined, CHECK_WIDTH);
-      // assert that the new state's property is equal to 'FULLSIZE'
       expect(newState.mainDisplay.displayMode).toEqual('FULLSIZE');
     });
 
-    // test: should save to state 'MIDSIZE' for widths between 1200px and 750px
     test('should save to state \'MIDSIZE\' for widths between 1200px and 750px', () => {
-      // change window.innerWidth to a resolution between 1200px and 750px
       window.innerWidth = 1000;
-      // invoke action creator for CHECK_WIDTH
       const CHECK_WIDTH = checkWidth();
-      // pass into photoApp the initial state and action
       const newState = photoApp(undefined, CHECK_WIDTH);
-      // assert that the new state's property is equal to 'FULLSIZE'
       expect(newState.mainDisplay.displayMode).toEqual('MIDSIZE');
     });
 
-    // test: should save to state 'COMPACT' for widths less than 750px
     test('should save to state \'COMPACT\' for widths less than 750px', () => {
-      // change window.innerWidth to a resolution less than 750px
-      window.innerWidth = 1300;
-      // invoke action creator for CHECK_WIDTH
+      window.innerWidth = 700;
       const CHECK_WIDTH = checkWidth();
-      // pass into photoApp the initial state and action
       const newState = photoApp(undefined, CHECK_WIDTH);
-      // assert that the new state's property is equal to 'FULLSIZE'
-      expect(newState.mainDisplay.displayMode).toEqual('FULLSIZE');
+      expect(newState.mainDisplay.displayMode).toEqual('COMPACT');
     });
 
   });
 
-  // Describe: FOCUS_IMAGE
+  
   describe('FOCUS_IMAGE', () => {
-    // test: should save to state (index) when given payload (index)
+    
     test('should save the given number to the inFocus property', () => {
-      // create a variable to hold the desired index
       const index = 3;
-      // invoke action creator for FOCUS_IMAGE with the created variable
       const FOCUS_IMAGE = focusImage(index);
-      // pass into photoApp a valid state and the created action
       let newState = photoApp(undefined, FOCUS_IMAGE);
-      // assert that the new state's property is equal to the index passed
       expect(newState.mainDisplay.inFocus).toEqual(index);
     });
 
-    // test: should only work for integers between 0 and 4 for displayMode 'FULLSIZE'
+    
     test('should only work for integers between 0 and 4 for displayMode \'FULLSIZE\'', () => {
       window.innerWidth = 1600;
       let CHECK_WIDTH = checkWidth();
-      // create a variable to hold an index between 0 and 4
-      let index = 4;
-      // create an action object of FOCUS_IMAGE with index 4
-      let FOCUS_IMAGE = focusImage(index);
-      // pass into photoApp a state object and the action object
       let newState = photoApp(undefined, CHECK_WIDTH);
+
+      let index = 4;
+      let FOCUS_IMAGE = focusImage(index);
       newState = photoApp(newState, FOCUS_IMAGE);
-      // assert that the new state's property is equal to 4
       expect(newState.mainDisplay.inFocus).toEqual(index);
 
-      // create a variable to hold an index of 9
       index = 9;
-      // create an action object of FOCUS_IMAGE
       FOCUS_IMAGE = focusImage(index);
-      // pass into photoApp a state object and the action object
       newState = photoApp(undefined, FOCUS_IMAGE);
-      // assert that the new state's property is equal to 'NONE'
       expect(newState.mainDisplay.inFocus).toEqual('NONE');
     });
 
-    // test: should only work for integers between 0 and 2 for displayMode 'MIDSIZE'
+    
     test('should only work for integers between 0 and 2 for displayMode \'MIDSIZE\'', () => {
       window.innerWidth = 1000;
       let CHECK_WIDTH = checkWidth();
-      // create a variable to hold an index between 0 and 2
-      let index = 2;
-      // create an action object of FOCUS_IMAGE with index 2
-      let FOCUS_IMAGE = focusImage(index);
-      // pass into photoApp a state object and the action object
       let newState = photoApp(undefined, CHECK_WIDTH);
-      newState = photoApp(newState, FOCUS_IMAGE);
-      // assert that the new state's property is equal to 2
-      expect(newState.mainDisplay.inFocus).toEqual(index);
 
-      // create a variable to hold an index of 4
-      index = 4;
-      // create an action object of FOCUS_IMAGE
-      FOCUS_IMAGE = focusImage(index);
-      // pass into photoApp a state object and the action object
+      let index = 2;
+      let FOCUS_IMAGE = focusImage(index);
       newState = photoApp(newState, FOCUS_IMAGE);
-      // assert that the new state's property is equal to 'NONE'
+      expect(newState.mainDisplay.inFocus).toEqual(index);
+      
+      index = 4;     
+      FOCUS_IMAGE = focusImage(index);
+      newState = photoApp(newState, FOCUS_IMAGE);
       expect(newState.mainDisplay.inFocus).toEqual('NONE');
     });
 
-    // test: should not work for displayMode 'COMPACT'
+    
     test('should not work for displayMode \'COMPACT\'', () => {
       window.innerWidth = 700;
       const CHECK_WIDTH = checkWidth();
-      // create a variable to hold an index of 2
       const index = 2;
-      // create an action object of FOCUS_IMAGE
       const FOCUS_IMAGE = focusImage(index);
-      // pass into photoApp a state object and the action object
       let newState = photoApp(undefined, CHECK_WIDTH);
       newState = photoApp(newState, FOCUS_IMAGE);
-      // assert that the new state's property is equal to 'NONE'
       expect(newState.mainDisplay.inFocus).toEqual('NONE');
     });
   });
 
-  // Describe: DEFOCUS_ALL
+  
   describe('DEFOCUS_ALL', () => {
-    // test: should save to state 'NONE' when given DEFOCUS_ALL action
+    
     test('should save to inFocus property \'NONE\'', () => {
-      // create an action object of DEFOCUS_ALL
       const DEFOCUS_ALL = defocusAll();
-      // pass into photoApp a state object and the action object
       const workingState = mockState([{}], 'FULLSIZE', 3, false, 'NONE');
       const newState = photoApp(workingState, DEFOCUS_ALL);
-      // assert that the new state's property is equal to 'NONE'
       expect(newState.mainDisplay.inFocus).toEqual('NONE');
     });
   });
