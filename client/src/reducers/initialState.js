2e5457f1fe38ff65ad93ee7e1e231a4df2758ffe
Kristoffer U Enriquez <enriquezkristoffer@yahoo.com>
2019-04-04 14:58:37 -0700

    Add initial state file that describes initial state for root reducer

---
+++ client/src/reducers/initialState.js
@@ -0,0 +1,13 @@
+const initialState = {
+  mainDisplay: {
+    photos: [],
+    displayMode: 'COMPACT',
+    inFocus: 'NONE',
+  },
+  modal: {  
+    modal: false,
+    modalDisplay: 'NONE',
+  },
+};
+
+export default initialState;
