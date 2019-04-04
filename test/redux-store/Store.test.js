2907b9eed7ff98689a61faa377e716a8d8b6c1f2
Kristoffer U Enriquez <enriquezkristoffer@yahoo.com>
2019-04-03 18:02:26 -0700

    Revise test file names for greater specificity and pertinence to a singular unit

---
+++ test/redux-store/Store.test.js
@@ -0,0 +1,8 @@
+/**
+ * Store test script
+ * Tests basic state functionality, without component integration
+ */
+
+test('can set multiple entry points for webpack to bundle multiple spec files', () => {
+  expect(true).toBe(true); // simplest possible friggin' test
+});
