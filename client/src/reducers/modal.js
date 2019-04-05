6c46b391fb2bfff61f83bd4272d701440be5e532
Kristoffer U Enriquez <enriquezkristoffer@yahoo.com>
2019-04-05 11:49:58 -0700

    Remove pseudocode from source file and clean up linebreaks

--- client/src/reducers/modal.js
+++ client/src/reducers/modal.js
@@ -3,9 +3,6 @@
  * Reducer for actions concerned with affecting change to the fullscreen modal
  */
 
-// TODO:
-// Write a reducer that accepts the following actions and handles them accordingly:
-
 const initialState = {
   photos: [],
   onScreen: false,
@@ -29,7 +26,6 @@ function modal(state = initialState, action) {
       return newState;
     case 'TOGGLE_MODAL':
       onScreen = !state.onScreen;
-
       if (onScreen === true) {
         if (action.photo > 0 && action.photo < state.photos.length) {
           ({ photo } = action);
@@ -39,40 +35,27 @@ function modal(state = initialState, action) {
       } else {
         photo = 'NONE';
       }
-
       newState = Object.assign({}, state, {
         onScreen,
         photo,
       });
       return newState;
     case 'MODAL_SELECT':
-
-      // if action.photo is 'NEXT' && state.photo > 0...
       if (action.photo === 'NEXT') {
-        // save the value of the state property to variable `photo`
         ({ photo } = state);
-        // state.photo is less than state.photos.length - 1, increment photo by one
         if (state.photo < state.photos.length - 1) {
           photo += 1;
         }
-      // else if action.photo is 'PREV' && state.photo < state.photos.length...
       } else if (action.photo === 'PREV') {
-        // save the vavlue of the state property to variable `photo`
         ({ photo } = state);
-        // decrement photo by one
         if (state.photo > 0) {
           photo -= 1;
         }
-      // else if action.photo < state.photos.length && action.photo > 0...
       } else if (action.photo < state.photos.length && action.photo > 0) {
-        // destructure the photo property of the action object
         ({ photo } = action);
-      // else
       } else {
-        // save 0 to photo
         photo = 0;
       }
-
       newState = Object.assign({}, state, {
         photo,
       });
