1b7a006c29fbe6426edaaa145dba63697f4a625d
Kristoffer U Enriquez <enriquezkristoffer@yahoo.com>
2019-04-05 16:34:36 -0700

    Create source and test files for mainDisplay subcomponents, write test stubs into mainDisplay test

---
+++ client/src/components/PhotoWrapperRow.jsx
@@ -0,0 +1,13 @@
+/**
+ * Photo wrapper row component
+ * Wrapper that places all elements in a row
+ */
+
+ /**
+  * TODO:
+  * 
+  * Write a component that has the following style properties:
+  *   flex display, column
+  *   full width and height
+  *   1px top and bottom padding
+  */
---
+++ test/components/mainDisplay.test.js
@@ -0,0 +1,25 @@
+/**
+ * Main display element test file
+ * Tests wrappers for images to be held in the main display for the item detail page
+ */
+
+import React from 'react';
+import { shallow } from 'enzyme';
+
+// import PhotoWrapper from '../../client/src/components/PhotoWrapper';
+// import PhotoWrapperColumn from '../../client/src/components/PhotoWrapperColumn';
+// import PhotoWrapperRow from '../../client/src/components/PhotoWrapperRow';
+
+describe('photo-wrapper component', () => {
+  test('should have exactly one img tag as its child', () => {});
+
+  test('should invoke a handler onClick', () => {});
+
+  test('should invoke a handler onMouseEnter', () => {});
+
+  test('should invoke a handler onMouseLeave', () => {});
+});
+
+describe('photo-wrapper-column component', () => {
+  test('should collapse from view when given the prop collapse', () => {});
+});
