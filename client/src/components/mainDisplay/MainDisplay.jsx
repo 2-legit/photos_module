8747d426db0d2b873acdb6230728b6d8938c9af5
Kristoffer U Enriquez <enriquezkristoffer@yahoo.com>
2019-04-06 06:50:40 -0700

    Add empty MainDisplay file to components folder

---
+++ client/src/components/mainDisplay/MainDisplay.jsx
@@ -0,0 +1,18 @@
+/**
+ * Main Display component
+ * Renders photo gallery using smaller subcomponents
+ */
+
+/**
+ * TODO:
+ * 
+ * Create a React component with the following style properties:
+ * 
+ *  full width, 30% height
+ *  bg color: very dark gray
+ * 
+ * Invoke:
+ *  PhotoWrapper
+ *  PhotoWrapperRow
+ *  PhotoWrapperCol
+ */
