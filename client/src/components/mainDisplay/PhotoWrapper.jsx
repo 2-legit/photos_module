a87481503996a523d9c70f504338898696ba4eec
Kristoffer U Enriquez <enriquezkristoffer@yahoo.com>
2019-04-06 21:34:32 -0700

    Add linebreaks to the ends of files, remove comments from component files

--- client/src/components/mainDisplay/PhotoWrapper.jsx
+++ client/src/components/mainDisplay/PhotoWrapper.jsx
@@ -3,22 +3,6 @@
  * Wrapper that holds a single image tag and prevents overflow
  */
 
- /**
-  * TODO:
-  * 
-  * Create a div that has the following style properties:
-  *   flex grow 1
-  *   hide overflow
-  *   1px margin
-  * 
-  *   <PROP CONDITION> if index === ownId && css
-  *     opacity 0.77
-  * 
-  * Within that div, have a single image tag that has the following style properties:
-  *   100% width and height
-  *   object-fit cover
-  */
-
   import React from 'react';
   import styled from 'styled-components';
 
