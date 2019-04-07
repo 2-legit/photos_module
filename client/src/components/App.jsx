41b835da85f98d77e6e366dfbb51fdd958a9cd64
Kristoffer U Enriquez <enriquezkristoffer@yahoo.com>
2019-04-06 22:10:27 -0700

    Debug MainDisplay component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -7,9 +7,13 @@ import React from 'react';
 
 import MainDisplay from './mainDisplay/MainDisplay.jsx';
 
+import photos from '../../../data/testPhotoData.js';
+
+const imageurls = photos.map(image => image.imageurl);
+
 function App() {
   return (
-    <MainDisplay photos={[]} />
+    <MainDisplay photos={imageurls} displayMode={2} />
   );
 }
 
