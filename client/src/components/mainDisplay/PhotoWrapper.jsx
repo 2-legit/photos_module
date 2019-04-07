b670cac4ddecef839492ebe4730f92d06aa192c6
Kristoffer U Enriquez <enriquezkristoffer@yahoo.com>
2019-04-06 23:45:04 -0700

    Connect MainDisplay component to Redux store

--- client/src/components/mainDisplay/PhotoWrapper.jsx
+++ client/src/components/mainDisplay/PhotoWrapper.jsx
@@ -3,30 +3,30 @@
  * Wrapper that holds a single image tag and prevents overflow
  */
 
-  import React from 'react';
-  import styled from 'styled-components';
+import React from 'react';
+import styled from 'styled-components';
 
-  const StyledWrapper = styled.div`
-    flex-grow: 1;
-    overflow: hidden;
-    margin: 1px;
-    opacity: ${props => (props.inFocus === 'NONE' || props.inFocus === props.id) ? '1' : '0.5'};
-  `;
+const StyledWrapper = styled.div`
+  flex-grow: 1;
+  overflow: hidden;
+  margin: 1px;
+  opacity: ${props => (props.inFocus === 'NONE' || props.inFocus === props.id) ? '1' : '0.5'};
+`;
 
-  StyledWrapper.displayName = 'StyledWrapper';
+StyledWrapper.displayName = 'StyledWrapper';
 
-  function PhotoWrapper(props) {
-    return (
-      <StyledWrapper inFocus={props.inFocus} id={props.id}>
-        <img
-        style={{width: '100%', height: '100%', objectFit: 'cover'}}
-        src={props.imageurl}
-        onClick={props.onPhotoClick}
-        onMouseEnter={props.onPhotoHover}
-        onMouseLeave={props.onPhotoLeave}
-        />
-      </StyledWrapper>
-    );
-  }
+function PhotoWrapper(props) {
+  return (
+    <StyledWrapper inFocus={props.inFocus} id={props.id}>
+      <img
+      style={{width: '100%', height: '100%', objectFit: 'cover'}}
+      src={props.imageurl}
+      onClick={props.onPhotoClick}
+      onMouseEnter={props.onPhotoHover}
+      onMouseLeave={props.onPhotoLeave}
+      />
+    </StyledWrapper>
+  );
+}
 
-  export default PhotoWrapper;
+export default PhotoWrapper;
