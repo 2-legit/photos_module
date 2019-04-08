4bd2d975abc39a3f5cf774b136dece10356f48a9
Kristoffer U Enriquez <enriquezkristoffer@yahoo.com>
2019-04-08 02:11:53 -0700

    Refine styling, add scrolling to PhotoReel

--- client/src/components/modal/Buttons.jsx
+++ client/src/components/modal/Buttons.jsx
@@ -6,39 +6,47 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import CLOSE_BUTTON from '../../../img/closeButton.png';
+import NEXT_BUTTON from '../../../img/nextButton.png';
+import PREV_BUTTON from '../../../img/prevButton.png';
+
 const CloseButton = styled.button`
   position: fixed;
   width: 70px;
   height: 70px;
   top: 20px;
   right: 20px;
-  background-color: red;
+  background-color: transparent;
   border: 0px solid;
 `;
 
 export function Close(props) {
   return (
-    <CloseButton
-    // img={}
-    onClick={props.onButtonClick}
-    />
+    <CloseButton onClick={props.onButtonClick}>
+      <img
+      style={{objectFit: 'contain', width: '100%', height: '100%'}}
+      src={CLOSE_BUTTON}
+      />
+    </CloseButton>
   );
 }
 
 const ArrowButton = styled.button`
   width: 70px;
   height: 70px;
-  background-color: red;
+  background-color: transparent;
   border: 0px solid;
 `;
 
 export function Next(props) {
   return (
     <a href={`#nav-${props.photo + 1}`}>
-    <ArrowButton
-    // img={}
-    onClick={() => props.onButtonClick('NEXT')}
-    />
+      <ArrowButton onClick={() => props.onButtonClick('NEXT')}>
+        <img
+        style={{objectFit: 'contain', width: '100%', height: '100%'}}
+        src={NEXT_BUTTON}
+        />
+      </ArrowButton>
     </a>
   );
 }
@@ -46,10 +54,12 @@ export function Next(props) {
 export function Prev(props) {
   return (
     <a href={`#nav-${props.photo - 1}`}>
-    <ArrowButton
-    // img={}
-    onClick={() => props.onButtonClick('PREV')}
-    />
+      <ArrowButton onClick={() => props.onButtonClick('PREV')}>
+        <img
+        style={{objectFit: 'contain', width: '100%', height: '100%'}}
+        src={PREV_BUTTON}
+        />
+      </ArrowButton>
     </a>
   );
 }
@@ -58,7 +68,7 @@ const PhotoWrapper = styled.div`
   width: 100px;
   height: 70px;
   overflow: hidden;
-  opacity: ${props => props.current === props.photo ? 1 : 0.5}
+  opacity: ${props => props.current === props.photo ? 1 : 0.5};
   flex-grow: 0;
   flex-shrink: 0;
   margin-left: 10px;
