0ca8dfa99e14531328c1034c8a2f55ff86fcb334
Kristoffer U Enriquez <enriquezkristoffer@yahoo.com>
2019-04-10 15:02:11 -0700

    Fix syntactical and style deviations

--- data/grabImages.js
+++ data/grabImages.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 const fs = require('fs');
 const path = require('path');
 
-fs.writeFileAsync = function(path, data, options) {
+fs.writeFileAsync = function (path, data, options) {
   return new Promise((resolve, reject) => {
     fs.writeFile(path, data, options, (error, data) => {
       if (error) {
@@ -11,22 +11,22 @@ fs.writeFileAsync = function(path, data, options) {
       resolve(data);
     });
   });
-}
+};
 
-let allPics = [];
+const allPics = [];
 let soonToBePic;
 
-for(let i = 0; i < 50; i += 1) {
-  soonToBePic = axios.get(`http://loremflickr.com/640/480/house,room?random=${i}`, { 
+for (let i = 0; i < 50; i += 1) {
+  soonToBePic = axios.get(`http://loremflickr.com/640/480/house,garden?random=${i}`, {
     responseType: 'arraybuffer',
     headers: {
       'Content-Type': 'image/jpeg',
     },
   })
-  .then(result => fs.writeFileAsync(path.join(__dirname, `photos/room-${i}.jpg`), result.data))
-  .catch(error => console.error('You done goofed!', error));
+    .then(result => fs.writeFileAsync(path.join(__dirname, `photos/room-${i}.jpg`), result.data))
+    .catch(error => console.error('You done goofed!', error));
   allPics.push(soonToBePic);
 }
 
 Promise.all(allPics)
-.then(() => console.log('All done!'));
+  .then(() => console.log('All done!'));
