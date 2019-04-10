918b8dc0ff133b620f2ad2b6782496e1ee9767d5
Kristoffer U Enriquez <enriquezkristoffer@yahoo.com>
2019-04-10 10:41:32 -0700

    Write grabImages to download photos from a website

---
+++ data/grabImages.js
@@ -0,0 +1,32 @@
+const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
+
+fs.writeFileAsync = function(path, data, options) {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(path, data, options, (error, data) => {
+      if (error) {
+        reject(error);
+      }
+      resolve(data);
+    });
+  });
+}
+
+let allPics = [];
+let soonToBePic;
+
+for(let i = 0; i < 50; i += 1) {
+  soonToBePic = axios.get(`http://loremflickr.com/640/480/house,room?random=${i}`, { 
+    responseType: 'arraybuffer',
+    headers: {
+      'Content-Type': 'image/jpeg',
+    },
+  })
+  .then(result => fs.writeFileAsync(path.join(__dirname, `photos/room-${i}.jpg`), result.data))
+  .catch(error => console.error('You done goofed!', error));
+  allPics.push(soonToBePic);
+}
+
+Promise.all(allPics)
+.then(() => console.log('All done!'));
