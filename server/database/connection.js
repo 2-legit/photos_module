004b1f3038831317faa6a78e91244d1952bbbc3e
Kristoffer U Enriquez <enriquezkristoffer@yahoo.com>
2019-04-10 19:43:51 -0700

    Change database connection, change routes, change AJAX call route in MainDisplay.jsx

--- server/database/connection.js
+++ server/database/connection.js
@@ -5,6 +5,12 @@ const { DB_USER, DB_PASS } = require('./pass.js');
 const sequelize = new Sequelize(null, DB_USER, DB_PASS, {
   host: 'localhost',
   dialect: 'mysql',
+  logging: false,
+  pool: {
+    max: 5,
+    idle: 30000,
+    acquire: 60000,
+  }
 });
 
 module.exports = sequelize;
