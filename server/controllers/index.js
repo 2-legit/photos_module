3886fbf57a76a6ea3cabd2b5f474faf48751446f
Kristoffer U Enriquez <enriquezkristoffer@yahoo.com>
2019-05-16 11:10:40 -0700

    Remove console log statements

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -7,12 +7,10 @@ module.exports = {
       sequelize.query('USE photo')
         .then(() => Photo.findAll({ where: { roomid: req.params.roomid } }))
         .then((result) => {
-          console.log(`Query made for ${req.params.roomid}`);
           res.set('Access-Control-Allow-Origin', '*');
           res.send({ photos: result });
         })
         .catch((error) => {
-          console.error(error);
           res.status(500).end();
         });
     },
