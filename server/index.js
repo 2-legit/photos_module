7919a2e0dd16ae6729dab9f7ac55107f6dd2f1c6
Kristoffer U Enriquez <enriquezkristoffer@yahoo.com>
2019-06-03 12:29:00 -0700

    Add compression middleware to server app file

--- server/index.js
+++ server/index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const path = require('path');
+const compression = require('compression');
+
 const router = require('./router');
 const initializeDatabase = require('./database/init.js');
 const sequelize = require('./database/connection.js');
 
 const app = express();
 
-app.use('/location/:locationid', express.static(path.join(__dirname, '/../public')));
+app.use(compression());
+
+app.use('/location/:locationid', express.static(path.join(__dirname, '/../public/')));
 
 app.use('/photos/byroom', router);
 
@@ -14,5 +18,5 @@ const port = 3002;
 
 app.listen(port, () => {
   console.log('[Server] Now listening on port', port);
-  initializeDatabase().then(() => {});
+  initializeDatabase();
 });
