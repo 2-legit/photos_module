2d95d11fcfb8549f9aaab2efcd3c203389ce7cec
Kristoffer Enriquez <enriquezkristoffer@yahoo.com>
2019-07-16 10:30:34 -0700

    Set controller to use database before query

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,9 +1,10 @@
+const database = require('../database/connection.js');
 const Photo = require('../model/photos.js');
 
 module.exports = {
   photos: {
     get: (req, res) => {
-      Photo.sync()
+      database.query('USE photo')
         .then(() => Photo.findAll({ where: { roomid: req.params.roomid } }))
         .then((result) => {
           res.set('Access-Control-Allow-Origin', '*');
