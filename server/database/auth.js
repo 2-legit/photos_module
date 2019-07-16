0ba85bb71d59aa31688e46f6e7a9b2e491896ab6
Kristoffer Enriquez <enriquezkristoffer@yahoo.com>
2019-07-16 13:07:12 -0700

    Add prompt for database hostname

--- server/database/auth.js
+++ server/database/auth.js
@@ -1,10 +1,11 @@
 const readlineSync = require('readline-sync');
 
-const auth = function () {
+function auth() {
+  process.env.DB_HOST = process.env.DB_HOST || readlineSync.question('Hostname: ');
   process.env.DB_USER = process.env.DB_USER || readlineSync.question('Username: ');
   process.env.DB_PASS = process.env.DB_PASS || readlineSync.question('Password: ', {
     hideEchoBack: true,
   });
-};
+}
 
 module.exports = auth;
